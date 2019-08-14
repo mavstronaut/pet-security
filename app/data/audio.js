15c82b5d783e6ade93a952fc59e53a82466ef56d
mavstronaut <mavbarona@gmail.com>
2019-08-13 21:16:14 -0400

    adding new structure

---
+++ app/data/audio.js
@@ -0,0 +1,64 @@
+var audioArray = [
+    {
+        name: 'dog-whistle-low-high',
+        type: 'distract',
+        ytlink: 'https://www.youtube.com/watch?v=qMwUZ6j3PV8',
+    },
+    {
+        name: 'birds-chirping',
+        type: 'good',
+        ytlink: 'https://www.youtube.com/watch?v=Qm846KdZN_c'
+    },
+    {
+        name: 'dog-whistle',
+        type: 'bad',
+        ytlink: 'https://www.youtube.com/watch?v=gfxewnndNQ8'
+    },
+    {
+        name: 'mosquito-tone',
+        type: 'bad',
+        ytlink: 'https://www.youtube.com/watch?v=GyNcpW8o-i4'
+    },
+    {
+        name: 'trevor-hall',
+        type: 'music',
+        ytlink: 'https://www.youtube.com/watch?v=XwHVS7Q5cOI'
+    },
+    {
+        name: 'The-Adventure-Zone',
+        type: 'podcast',
+        ytlink: 'https://www.youtube.com/watch?v=HfzGrlNXuhk'
+    },
+    {
+        name: 'audience-cheer',
+        type: 'distract',
+        ytlink: 'https://www.youtube.com/watch?v=tq-Bp_2FOGA'
+    },
+    {
+        name: 'air-horn',
+        type: 'distract',
+        ytlink: 'https://www.youtube.com/watch?v=OFr74zI1LBM'
+    },
+    {
+        name: 'fog-horn',
+        type: 'distract',
+        ytlink: 'https://www.youtube.com/watch?v=8dENYJbN1z4'
+    },
+    {
+        name: 'whistle',
+        type: 'distract',
+        ytlink: 'https://www.youtube.com/watch?v=9UBxlyCk8RE'
+    },
+    {
+        name: 'hmm-all',
+        type: 'distract',
+        ytlink: 'https://www.youtube.com/watch?v=hNEmxbdKx9k'
+    },
+    {
+        name: 'beeps',
+        type: 'bad',
+        ytlink: 'https://www.youtube.com/watch?v=Dq196N-mHSE'
+    }
+];
+
+module.exports = audioArray;
